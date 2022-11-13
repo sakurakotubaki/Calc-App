4b7c1f9b2e77d640d936f6605d7b102f478f414c
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-11-13 12:10:28 +0900

    [change]
    Chakura UIでLayoutを整えた

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Box, ChakraProvider, Input } from "@chakra-ui/react";
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
 
   //  入力フォームのボタンの機能を使う関数
   const handleChangeCalc = (e: any) => {
@@ -11,22 +13,21 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <p>1KB❎入力された数値で計算を行う</p>
-      <p>1KBは1024バイトです</p>
-      <p>{count}KBは</p>
-      <p>{count * 1024}バイトです</p>
-      <form>
-        <label>
-          数値を入力:
-          {/* inputタグに、入力された値を保存する関数を書く */}
-          <input type="text" name="name" value={count}
-          onChange={handleChangeCalc} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  )
+    <ChakraProvider>
+      <Box className="App">
+        <Box mt={10} mb={5} ml={5} p={5} w={300} bg='orange.200'>
+        <Box mb={3}>1KB×入力された数値で計算を行う</Box>
+        <Box mb={3}>1KBは1024バイトです</Box>
+        <Box mb={3}>{count}KBは</Box>
+        <Box mb={3}>{count * 1024}バイトです</Box>
+        </Box>
+        <FormControl>
+          <FormLabel ml={5} color='red.600'> 数値を入力:</FormLabel>
+          <Input w={200} ml={5} type="text" name="name" value={count} onChange={handleChangeCalc} />
+        </FormControl>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
